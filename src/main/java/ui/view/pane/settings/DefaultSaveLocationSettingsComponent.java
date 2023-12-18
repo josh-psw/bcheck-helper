90d1cd4896aeaca6893f22706a1ebab32758fcf0
Dolph Flynn <96876199+DolphFlynn@users.noreply.github.com>
2023-12-18 10:22:11 +0000

    Change action to 'Open' within Repository root directory dialog.

--- src/main/java/ui/view/pane/settings/DefaultSaveLocationSettingsComponent.java
+++ src/main/java/ui/view/pane/settings/DefaultSaveLocationSettingsComponent.java
@@ -9,7 +9,7 @@ import java.nio.file.Path;
 import java.util.Optional;
 
 import static java.awt.GridBagConstraints.FIRST_LINE_START;
-import static ui.view.component.filechooser.ChooseMode.DIRECTORIES_ONLY;
+import static ui.view.component.filechooser.ChooseMode.SAVE_DIRECTORIES_ONLY;
 
 class DefaultSaveLocationSettingsComponent implements SettingsComponent {
     private final JPanel component = new JPanel();
@@ -57,7 +57,7 @@ class DefaultSaveLocationSettingsComponent implements SettingsComponent {
         chooseFileButton.setVisible(potentialDefaultSaveLocation.isPresent());
         chooseFileButton.addActionListener(e -> {
             if (e.getSource() == chooseFileButton) {
-                Optional<Path> selectedDirectory = new FileChooser(DIRECTORIES_ONLY).prompt();
+                Optional<Path> selectedDirectory = new FileChooser(SAVE_DIRECTORIES_ONLY).prompt();
 
                 selectedDirectory.ifPresent(file -> {
                     String pathToDirectory = file.toAbsolutePath().toString();
