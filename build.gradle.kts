01a6fd4e6c270f29287b2d1f43aa81600c064fcb
Dolph Flynn <96876199+DolphFlynn@users.noreply.github.com>
2025-02-26 21:00:52 +0000

    Upgrade JUnit.

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,10 +11,12 @@ repositories {
 
 dependencies {
     compileOnly("net.portswigger.burp.extensions:montoya-api:2025.2")
-    testImplementation("org.junit.jupiter:junit-jupiter:5.11.4")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.12.0")
     testImplementation("org.assertj:assertj-core:3.27.3")
     testImplementation("org.mockito:mockito-core:5.15.2")
     testImplementation("net.portswigger.burp.extensions:montoya-api:2025.2")
+
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 java {
