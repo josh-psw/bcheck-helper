f23fc15f1eb6bd05f74856436d9f8bfd6359844c
Hannah Law <hannah.law@portswigger.net>
2025-02-28 20:27:21 +0000

    Introduce BambdaItemImporter but throw error.

---
+++ src/main/java/data/bambda/BambdaItemImporter.java
@@ -0,0 +1,10 @@
+package data.bambda;
+
+import data.ItemImporter;
+
+public class BambdaItemImporter implements ItemImporter<Bambda> {
+    @Override
+    public void importItem(Bambda item) {
+        throw new RuntimeException("Not implemented yet.");
+    }
+}
