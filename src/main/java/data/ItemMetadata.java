1656384435b306ebc9198690f48ae93ea0657a00
Hannah Law <hannah.law@portswigger.net>
2025-03-07 11:58:23 +0000

    Provide a prefix for persistence.

--- src/main/java/data/ItemMetadata.java
+++ src/main/java/data/ItemMetadata.java
@@ -4,74 +4,44 @@ public enum ItemMetadata implements RepositoryMetadata, SaveLocationMetadata, Na
     BCHECK(
             "BCheck",
             ".bcheck",
-            "bcheck-store",
-            "repository.type",
-            "github_settings.repo",
-            "github_settings.url",
-            "portswigger/bchecks",
-            "filesystem_repository.location",
-            "default_save_location.use_setting",
-            "default_save_location.save_location" ),
+            "bcheck",
+            "portswigger/bchecks"),
     BAMBDA(
             "Bambda",
             ".bambda",
-            "bambda-store",
-            "bambda.repository.type",
-            "bambda.github_settings.repo",
-            "bambda.github_settings.url",
-            "portswigger/bambdas",
-            "bambda.filesystem_repository.location",
-            "bambda.default_save_location.use_setting",
-            "bambda.default_save_location.save_location");
+            "bambda",
+            "portswigger/bambdas");
 
 
     private final String name;
     private final String fileExtension;
-    private final String tempDirectoryPrefix;
-    private final String useSettingKey;
-    private final String saveLocationKey;
-    private final String repositoryTypeKey;
-    private final String repositoryNameKey;
-    private final String repositoryUrlKey;
+    private final String settingPrefix;
     private final String defaultRepositoryNameValue;
-    private final String fileSystemRepositoryLocationKey;
 
     ItemMetadata(
             String name,
             String fileExtension,
-            String tempDirectoryPrefix,
-            String repositoryTypeKey,
-            String repositoryNameKey,
-            String repositoryUrlKey,
-            String defaultRepositoryNameValue,
-            String fileSystemRepositoryLocationKey,
-            String useSettingKey,
-            String saveLocationKey) {
+            String settingPrefix,
+            String defaultRepositoryNameValue) {
         this.name = name;
         this.fileExtension = fileExtension;
-        this.tempDirectoryPrefix = tempDirectoryPrefix;
-        this.repositoryTypeKey = repositoryTypeKey;
-        this.repositoryNameKey = repositoryNameKey;
-        this.repositoryUrlKey = repositoryUrlKey;
+        this.settingPrefix = settingPrefix;
         this.defaultRepositoryNameValue = defaultRepositoryNameValue;
-        this.fileSystemRepositoryLocationKey = fileSystemRepositoryLocationKey;
-        this.useSettingKey = useSettingKey;
-        this.saveLocationKey = saveLocationKey;
     }
 
     @Override
     public String getRepositoryTypeKey() {
-        return repositoryTypeKey;
+        return settingPrefix + ".repository.type";
     }
 
     @Override
     public String getRepositoryNameKey() {
-        return repositoryNameKey;
+        return settingPrefix + ".github_settings.repo";
     }
 
     @Override
     public String getRepositoryUrlKey() {
-        return repositoryUrlKey;
+        return settingPrefix + ".github_settings.url";
     }
 
     @Override
@@ -81,17 +51,17 @@ public enum ItemMetadata implements RepositoryMetadata, SaveLocationMetadata, Na
 
     @Override
     public String getFileSystemRepositoryLocationKey() {
-        return fileSystemRepositoryLocationKey;
+        return settingPrefix + ".filesystem_repository.location";
     }
 
     @Override
     public String getUseSettingKey() {
-        return useSettingKey;
+        return settingPrefix + ".default_save_location.use_setting";
     }
 
     @Override
     public String getSaveLocationKey() {
-        return saveLocationKey;
+        return settingPrefix + ".default_save_location.save_location";
     }
 
     @Override
@@ -101,7 +71,7 @@ public enum ItemMetadata implements RepositoryMetadata, SaveLocationMetadata, Na
 
     @Override
     public String getTempDirectoryPrefix() {
-        return tempDirectoryPrefix;
+        return settingPrefix + "-store";
     }
 
     @Override
