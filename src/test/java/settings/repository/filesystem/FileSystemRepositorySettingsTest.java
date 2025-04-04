e2e94bf315323e9ce34473a1b6dbf41a2fa597db
Hannah Law <hannah.law@portswigger.net>
2025-04-04 11:31:09 +0100

    Parameterize FileSystemRepositorySettingsTest.

--- src/test/java/settings/repository/filesystem/FileSystemRepositorySettingsTest.java
+++ src/test/java/settings/repository/filesystem/FileSystemRepositorySettingsTest.java
@@ -1,38 +1,44 @@
 package settings.repository.filesystem;
 
 import burp.api.montoya.persistence.Preferences;
-import org.junit.jupiter.api.Test;
+import data.ItemMetadata;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
 
-import static data.ItemMetadata.BCHECK;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.*;
 
 class FileSystemRepositorySettingsTest {
     private final Preferences preferences = mock(Preferences.class);
-    private final FileSystemRepositorySettings repositorySettings = new FileSystemRepositorySettings(preferences, BCHECK);
 
-    @Test
-    void givenNoLocationSet_whenRepositoryLocation_thenEmptyStringReturned() {
-        when(preferences.getString(BCHECK.getFileSystemRepositoryLocationKey())).thenReturn(null);
+    @ParameterizedTest
+    @EnumSource(ItemMetadata.class)
+    void givenNoLocationSet_whenRepositoryLocation_thenEmptyStringReturned(ItemMetadata itemMetadata) {
+        FileSystemRepositorySettings repositorySettings = new FileSystemRepositorySettings(preferences, itemMetadata);
+        when(preferences.getString(itemMetadata.getFileSystemRepositoryLocationKey())).thenReturn(null);
 
         assertThat(repositorySettings.repositoryLocation()).isEmpty();
     }
 
-    @Test
-    void givenLocationSet_whenRepositoryLocation_thenCorrectLocationReturned() {
+    @ParameterizedTest
+    @EnumSource(ItemMetadata.class)
+    void givenLocationSet_whenRepositoryLocation_thenCorrectLocationReturned(ItemMetadata itemMetadata) {
         String location = "/home/peter.wiener/bchecks";
+        FileSystemRepositorySettings repositorySettings = new FileSystemRepositorySettings(preferences, itemMetadata);
 
-        when(preferences.getString(BCHECK.getFileSystemRepositoryLocationKey())).thenReturn(location);
+        when(preferences.getString(itemMetadata.getFileSystemRepositoryLocationKey())).thenReturn(location);
 
         assertThat(repositorySettings.repositoryLocation()).isEqualTo(location);
     }
 
-    @Test
-    void whenSetRepositoryLocation_thenLocationSavedToCorrectKey() {
+    @ParameterizedTest
+    @EnumSource(ItemMetadata.class)
+    void whenSetRepositoryLocation_thenLocationSavedToCorrectKey(ItemMetadata itemMetadata) {
         String location = "/home/peter.wiener/bchecks";
+        FileSystemRepositorySettings repositorySettings = new FileSystemRepositorySettings(preferences, itemMetadata);
 
         repositorySettings.setRepositoryLocation(location);
 
-        verify(preferences).setString(BCHECK.getFileSystemRepositoryLocationKey(), location);
+        verify(preferences).setString(itemMetadata.getFileSystemRepositoryLocationKey(), location);
     }
 }
