b83d27d71ef552b345e139c8c3ef6ccd35632ff4
Hannah Law <hannah.law@portswigger.net>
2025-04-25 14:49:04 +0100

    Add import support for v2025.4+

--- src/main/java/data/bambda/BambdaItemImporter.java
+++ src/main/java/data/bambda/BambdaItemImporter.java
@@ -1,18 +1,38 @@
 package data.bambda;
 
+import burp.api.montoya.bambda.BambdaImportResult;
 import data.ItemImporter;
 import logging.Logger;
 
 public class BambdaItemImporter implements ItemImporter<Bambda> {
     private final Logger logger;
+    private final burp.api.montoya.bambda.Bambda bambdaApi;
 
-    public BambdaItemImporter(Logger logger) {
+    public BambdaItemImporter(Logger logger, burp.api.montoya.bambda.Bambda bambdaApi) {
         this.logger = logger;
+        this.bambdaApi = bambdaApi;
     }
 
     @Override
     public void importItem(Bambda item) {
-        logger.logError("Direct Bambda imports are not supported yet.\nRegister your interest for this functionality here: https://github.com/PortSwigger/burp-extensions-montoya-api/issues/112");
-        throw new IllegalStateException("Bambda import not yet supported!");
+        if (bambdaApi == null) {
+            logger.logError("Bambda import unsupported in this version of Burp. Please upgrade to v2025.4+");
+            throw new IllegalStateException("Bambda import is supported in v2025.4+");
+        }
+
+        BambdaImportResult importResult = bambdaApi.importBambda(item.content());
+
+        if (!importResult.importErrors().isEmpty()) {
+            StringBuilder sb = new StringBuilder("Error importing: ")
+                    .append(item.name())
+                    .append("\n");
+
+            importResult.importErrors()
+                    .forEach(s -> sb.append("\t").append(s).append("\n"));
+
+            logger.logError(sb.toString());
+
+            throw new IllegalStateException("Error importing item!");
+        }
     }
 }
